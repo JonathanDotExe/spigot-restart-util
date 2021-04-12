795e3e1e82a2e859d119c5fe1dab3278fc49b636
DatDonathan <jojo08082003@gmail.com>
2021-04-12 13:43:21 +0200

    Created spigot project

---
+++ src/at/jojokobi/restartutil/RestartUtil.java
@@ -0,0 +1,12 @@
+package at.jojokobi.restartutil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RestartUtil extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+
+}
